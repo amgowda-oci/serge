a97eaac0b0ea5421484c21065515befae666011a
Amar Gowda (OCI) <151788184+amgowda-oci@users.noreply.github.com>
2024-01-08 11:03:21 -0800

    changes with openblas build
    added nramar repo

--- Dockerfile
+++ Dockerfile
@@ -35,6 +35,8 @@ COPY vendor/requirements.txt /usr/src/app/requirements.txt
 # Install api dependencies
 RUN apt-get update \
     && apt-get install -y --no-install-recommends dumb-init \
+    && apt-get -y install libopenblas-dev \
+    && pip install --upgrade pip \
     && pip install --no-cache-dir ./api \
     && pip install -r /usr/src/app/requirements.txt \
     && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* \
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,9 @@ source serge.env
 # Get CPU Architecture
 cpu_arch=$(uname -m)
 
+# build with OpenBLAS=1
+blasconfig="CMAKE_ARGS=\"-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS\""
+
 # Function to detect CPU features
 detect_cpu_features() {
 	cpu_info=$(lscpu)
@@ -20,13 +23,12 @@ detect_cpu_features() {
 	fi
 }
 
-# Check if the CPU architecture is aarch64/arm64
 if [ "$cpu_arch" = "aarch64" ]; then
-	pip_command="python -m pip install -v llama-cpp-python==$LLAMA_PYTHON_VERSION --only-binary=:all: --extra-index-url=https://gaby.github.io/arm64-wheels/"
+	pip_command="$blasconfig pip install -v llama-cpp-python --only-binary=:all: --extra-index-url=https://gaby.github.io/arm64-wheels/"
 else
 	# Use @jllllll provided wheels
 	cpu_feature=$(detect_cpu_features)
-	pip_command="python -m pip install -v llama-cpp-python==$LLAMA_PYTHON_VERSION --only-binary=:all: --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/$cpu_feature/cpu"
+	pip_command="$blasconfigpip pip install -v llama-cpp-python --only-binary=:all: --extra-index-url=https://jllllll.github.io/llama-cpp-python-cuBLAS-wheels/$cpu_feature/cpu"
 fi
 
 echo "Recommended install command for llama-cpp-python: $pip_command"
